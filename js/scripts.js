3eda92a4764d172f5109d7883589514771da8e25
Aleksandra Szewczyk <aleksandra_szewczyk@o2.pl>
2017-02-03 23:04:59 +0100

    Exercise 8.3 finished

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,9 @@
+// komentarz liniowy
+// komentarz liniowy cz. 2
 var name = prompt('Wpisz swoje imię');
 // komentarz polecenia 'alert'
 alert('Witaj, ' + name);
 console.log('Witaj, ' + name);
+/*
+  tutaj można dodać komentarz blokowy
+*/
