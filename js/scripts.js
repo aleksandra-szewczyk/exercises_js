559f60bef65cd215baa9c710e00439b6411bca8b
Aleksandra Szewczyk <aleksandra_szewczyk@o2.pl>
2017-02-06 01:19:12 +0100

    triangleArea

--- js/scripts.js
+++ js/scripts.js
@@ -4,5 +4,5 @@ console.log('Witaj, ' + name);
 
 var a = prompt('Podaj długość podstawy trójkąta (a)');
 var h = prompt('Podaj wysokość trójkąta (h)');
-traingleArea = a * h / 2;
+triangleArea = a * h / 2;
 console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
