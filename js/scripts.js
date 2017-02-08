11723bc8fb4c080a20039c593286ca925f5640f9
Aleksandra Szewczyk <aleksandra_szewczyk@o2.pl>
2017-02-08 23:40:37 +0100

    Done exercise 8.6

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,13 @@
-var name = prompt('Wpisz swoje imię');
-// komentarz polecenia 'alert'
-alert('Witaj, ' + name);
-console.log('Witaj, ' + name);
+// plik scripts.js
+function getTriangleArea(a, h) {
+    if ((a <= 0) || (h <= 0)) {
+        console.log('Nieprawidłowe dane');
+    }
+    return a * h / 2;
+}
+
+console.log(getTriangleArea(10, 6));
+
+var triangle1Area = getTriangleArea(10, 15);
+var triangle2Area = getTriangleArea(0, 8);
+var triangle3Area = getTriangleArea(14, 17);
