85a51d25410d1cfd0bd057292979fc23fc9d2d3b
Aleksandra Szewczyk <aleksandra_szewczyk@o2.pl>
2017-02-09 14:29:04 +0100

    Changed function

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,10 @@
 // plik scripts.js
 function getTriangleArea(a, h) {
-    if ((a <= 0) || (h <= 0)) {
-        console.log('Nieprawidłowe dane');
-    }
-    return a * h / 2;
+    if ((a > 0) && (h > 0)) {
+        return a * h / 2;
+    } else {
+        return 'Nieprawidłowe dane';
+    };
 }
 
 console.log(getTriangleArea(10, 6));
