dd1488ab42d78ca37cb0459309eed4faddf79175
Aleksandra Szewczyk <aleksandra_szewczyk@o2.pl>
2017-02-11 23:01:23 +0100

    Done exercise 8.7

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,15 @@
-var name = prompt('Wpisz swoje imię');
-// komentarz polecenia 'alert'
-alert('Witaj, ' + name);
-console.log('Witaj, ' + name);
+var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
+var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
+var allNames = femaleNames.concat(maleNames);
+console.log('Tablica zawiera następujące imiona: ' + allNames);
+
+var newName ='Franek';
+var newName = prompt('Podaj nowe imię');
+
+if (allNames.indexOf(newName) === -1) {
+  allNames.push(newName);
+} else {
+  console.log('Podane imię znajduje się już w tablicy: ' + newName);
+}
+
+console.log('Ostatecznie tablica zawiera następujące imiona: ' + allNames)
