1183c9eebccf88d4b1f53b1c4d1e7fde4e4ffc04
Aleksandra Szewczyk <aleksandra_szewczyk@o2.pl>
2017-02-12 00:49:05 +0100

    Done exercise8.8

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
-var name = prompt('Wpisz swoje imię');
-// komentarz polecenia 'alert'
-alert('Witaj, ' + name);
-console.log('Witaj, ' + name);
+var text = 'Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach.';
+var animal = 'Zielone słonie';
+var animal = animal.toUpperCase();
+var text = text.replace('Papugi', animal);
+
+console.log(text.substr(0,text.length*0.5));
