9691c1375ad22add09e6630b70b8400644f05a52
Aleksandra Szewczyk <aleksandra_szewczyk@o2.pl>
2017-02-17 22:30:18 +0100

    Add button function

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,12 @@
-var name = prompt('Wpisz swoje imię');
-// komentarz polecenia 'alert'
-alert('Witaj, ' + name);
-console.log('Witaj, ' + name);
+var add = document.getElementById('addElem');
+var list = document.getElementById('list');
+var itemNumber = 1;
+
+function checkOnclickEvent(param) {
+  list.innerHTML += '<li>item ' + param + '</li>'
+}
+
+add.addEventListener('click', function(e) {
+    checkOnclickEvent(itemNumber);
+    itemNumber++;
+});
